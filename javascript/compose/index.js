c276dc183096c2ea1795ce38cadccaccfa416d00
yuanalexwu <yuanalexwu@gmail.com>
2018-07-15 23:05:43 +0800

    ✨ Add compose function

---
+++ javascript/compose/index.js
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) 2018. Suzhou DHMS Information Technology Co.,Ltd.
+ * Author: Wuyuan Created:2018/7/15
+ */
+const add1 = value => value + 1
+const multiply = value => value * 2
+
+// 1.
+const compose = (fn, ...rest) => rest.length === 0 ? fn : (...args) => fn(compose(...rest)(...args))
+const composedFunc1 = compose(multiply, add1)
+console.log('result: ', composedFunc1(10))
+
+// 2.
+const compose2 = (...funcs) => {
+    return funcs.reduce((f, g) => {
+        return (...args) => f(g(...args))
+    })
+}
+const composedFunc2 = compose2(multiply, add1)
+console.log('result2: ', composedFunc2(10))
